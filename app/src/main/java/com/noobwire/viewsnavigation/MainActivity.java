b93d42448e21fd4c0499bf71004f9965e840c5e9
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-10-19 16:19:18 -0300

    :sparkles: added intent

--- app/src/main/java/com/noobwire/viewsnavigation/MainActivity.java
+++ app/src/main/java/com/noobwire/viewsnavigation/MainActivity.java
@@ -3,9 +3,9 @@ package com.noobwire.viewsnavigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/noobwire/viewsnavigation/MenuActivity.java
+++ app/src/main/java/com/noobwire/viewsnavigation/MenuActivity.java
@@ -2,13 +2,36 @@ package com.noobwire.viewsnavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button btnDaisy;
+    Button btnLuigi;
+    Button btnRosalina;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        btnDaisy = findViewById(R.id.idCharmander);
+        btnLuigi = findViewById(R.id.idBulbasaur);
+        btnRosalina = findViewById(R.id.idSquirtle);
+
+        btnDaisy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, DescricaoActivity.class);
+                intent.putExtra("daisy", "daisy");
+
+                startActivity(intent);
+            }
+        });
+
     }
 }

3ad421d4e8557982569f99ca00f0323a0abfbd00
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-09 09:46:10 -0300

    :sparkles: dark mode handler

--- app/src/main/java/com/noobwire/viewsnavigation/MenuActivity.java
+++ app/src/main/java/com/noobwire/viewsnavigation/MenuActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
     Button btnDaisy;
     Button btnLuigi;
     Button btnRosalina;
+    TextView btnFechar;
+    TextView btnMudarTema;
+
+    boolean dark = false;
+
+    LinearLayout layout;
 
 
     @Override
@@ -20,9 +29,17 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
 
+
         btnDaisy = findViewById(R.id.idCharmander);
         btnLuigi = findViewById(R.id.idBulbasaur);
         btnRosalina = findViewById(R.id.idSquirtle);
+        btnFechar = findViewById(R.id.idFechar);
+        btnMudarTema = findViewById(R.id.idMudarTema);
+
+        layout = findViewById(R.id.idMenuLayout);
+
+        SharedPreferences prefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        dark = prefs.getBoolean("dark", false);
 
         btnDaisy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +58,23 @@ public class MenuActivity extends AppCompatActivity {
         btnRosalina.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {intentWithCharData("rosalina", DescricaoActivity.class); }
+        });
+
+        btnFechar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnMudarTema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("AppPrefs", MODE_PRIVATE).edit();
+                dark = !dark;
+                editor.putBoolean("dark", dark);
+                editor.apply();
+            }
         });
     }
 
